14286c3e23ce3de8c1dd3cfaaf277850c8c1bfe0
Keonhwi99 <keonhwi99>
2025-06-12 12:40:10 +0900

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { RouterProvider } from 'react-router-dom';
+import { Routers } from './routers/Routers.ts';
+
+createRoot(document.getElementById('root')!).render(
+  <RecoilRoot>
+    <RouterProvider router={Routers} />
+  </RecoilRoot>
+);
